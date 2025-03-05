1ff8e4f9cea9db4582140de21889ca69205a7afd
anish <anishwanare9@gmail.com>
2025-03-05 22:52:40 +0530

    buggiees

--- src/Admin/MyPublishBooks.jsx
+++ src/Admin/MyPublishBooks.jsx
@@ -6,6 +6,14 @@ import Card from '../component/Card'
 const MyPublishBooks = () => {
     const { myBooks, loading } = useSelector((state) => state.UploadBook)
 
+    if (loading) {
+        return (
+            <div className='max-w-6xl mx-auto py-6 px-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6'>
+                {Array.from({ length: 8 }).map(() => <Skeleton />)}
+            </div>
+        )
+    }
+
     return (
         <div className='max-w-6xl mx-auto py-6 px-4'>
             <h2 className="text-2xl font-bold text-gray-900 mb-6"></h2>
