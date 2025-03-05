bf553794f5a20e748b82a8be3b7a4161957f0a06
anish <anishwanare9@gmail.com>
2025-03-05 23:00:38 +0530

    buggiees

--- src/pages/Books.jsx
+++ src/pages/Books.jsx
@@ -17,7 +17,7 @@ const Books = () => {
 
     return (
         <div className='max-w-6xl mx-auto py-6 px-4'>
-            <h1 className="  md:my-10 text-center text-xl md:text-2xl font-semibold text-orange-500 cursor-pointer underline">Explore Books </h1>
+            <h1 className="  md:my-10 text-center text-xl md:text-2xl font-semibold text-orange-500 cursor-pointer underline">Explore Bookiesss</h1>
             <hr />
             <h2 className="text-2xl font-bold text-gray-900 mb-6"></h2>
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
